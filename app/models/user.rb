8da59ca06eb8dc02ef447285f9f2cb6fc3a586a0
Shaqri <peter200970@hotmail.com>
2021-09-24 20:31:40 -0500

    Add tests for user model and change password system

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,20 +1,12 @@
 class User < ApplicationRecord
   include BCrypt
   validates :name, presence: true, length: {minimum: 6, maximum: 20}
-  validates :email, presence: true, uniqueness: {case_sensitive: true}
-  validates :password_hash, presence: true
-  validates :password, confirmation: true
-  validates :password_confirmation, presence: true
+  validates :email, presence: true, uniqueness: {case_sensitive: true}, length: {minimum: 13}
+  validates :password_digest, presence: true, length: {minimum: 6}
   validates :country, presence: true
 
+  has_secure_password
+
   has_one :shopping_cart
-
-  def password
-    @password ||= Password.new(password_hash)
-  end
-
-  def password=(new_password)
-    @password = Password.create(new_password)
-    self.password_hash = @password
-  end
+  
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,21 +13,8 @@ RSpec.describe User, type: :model do
     it {should validate_length_of(:name)}
     it {should validate_presence_of(:email)}
     it {should validate_uniqueness_of(:email)}
-    it {should validate_presence_of(:password_hash)}
-    it {should validate_confirmation_of(:password)}
-    it {should validate_presence_of(:password_confirmation)}
+    it {should validate_presence_of(:password_digest)}
+    it {should have_secure_password}
     it {should validate_presence_of(:country)}
-  end
-
-  describe '#password' do
-    it "return encripted password using bcrypt" do
-      expect(subject.password.class).to eq(BCrypt::Password)
-    end
-  end 
-  describe '#password=' do
-    it "create encripted password using bycrypt and assign it to user password_hash" do
-      subject.password = 'new_password'
-      expect(subject.password_hash).to be_truthy
-    end
   end
 end
